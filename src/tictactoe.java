900d30ebd7366a3933400d418fe95b9a2f2da608
Leo Omnes <l.omnes@lprs.fr>
2024-12-12 14:04:38 +0100

    first commit, creation du projet

---
+++ src/tictactoe.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class tictactoe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Joueur 1 (la croix): ");
+        String joueur1 = sc.nextLine();
+        joueur1=joueur1.trim();
+        System.out.print("Joueur 2 (le rond): ");
+        
+    }
+}
